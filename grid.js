9544d2c163773b6e4912f2d9317f466dfd9531b7
Roman Zotsenko <romazotsenko15@gmail.com>
2025-04-19 22:30:12 +0200

    grid with javascript

---
+++ grid.js
@@ -0,0 +1,15 @@
+import { Cell } from "./cell.js";
+
+const GRID_SIZE = 4;
+const CELLS_COUNT = GRID_SIZE * GRID_SIZE;
+
+export class Grid {
+    constructor(gridElement) {
+        this.cells = [];
+        for (let i = 0; i < CELLS_COUNT; i++) {
+            this.cells.push(
+                new Cell(gridElement, i % GRID_SIZE, Math.floor(i / GRID_SIZE))
+            );
+        }
+    }
+}

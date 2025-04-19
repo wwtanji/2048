304acc54974e6ae45f7a439d23fab6fd5af7807e
Roman Zotsenko <romazotsenko15@gmail.com>
2025-04-19 22:48:17 +0200

    Implement grid movement and tile linking functionality

--- tile.js
+++ tile.js
@@ -1,11 +1,11 @@
 export class Tile {
-    constructor(gridElement) {
-        this.tileElement = document.createElement("div");
-        this.tileElement.classList.add("tile");
-        this.setValue(Math.random() > 0.5 ? 2 : 4);
-        gridElement.append(this.tileElement);
-    }
-  
+  constructor(gridElement) {
+    this.tileElement = document.createElement("div");
+    this.tileElement.classList.add("tile");
+    this.setValue(Math.random() > 0.5 ? 2 : 4);
+    gridElement.append(this.tileElement);
+  }
+
   setValue(value) {
     this.value = value;
     this.tileElement.textContent = value;
@@ -19,5 +19,23 @@ export class Tile {
     this.y = y;
     this.tileElement.style.setProperty("--x", x);
     this.tileElement.style.setProperty("--y", y);
+  }
+
+  removeFromDOM() {
+    this.tileElement.remove();
+  }
+
+  waitForTransitionEnd() {
+    return new Promise(resolve => {
+      this.tileElement.addEventListener(
+        "transitionend", resolve, { once: true });
+    });
+  }
+
+  waitForAnimationEnd() {
+    return new Promise(resolve => {
+      this.tileElement.addEventListener(
+        "animationend", resolve, { once: true });
+    });
   }
 }
